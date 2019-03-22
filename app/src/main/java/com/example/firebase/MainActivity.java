9a753aa5080a62df9422d78a3617ab2cf997c494
MartinDeep13 <martin0013esp@hotmail.com>
2019-03-22 07:07:02 -0600

    Initial commit

---
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.firebase;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    EditText correo, contra;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        correo=findViewById(R.id.correo);
+        contra = findViewById(R.id.contra);
+    }
+
+    public void iniciar(View view) {
+        String email = correo.getText().toString();
+        String password = contra.getText().toString();
+        Log.d("valo",email);
+        if(email.length()!=0 && password.length()!=0)
+        {
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful())
+                    {
+                        Toast.makeText(MainActivity.this,"Usuario o passwor erroneos", Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this,"Se ha iniciado correctamente",Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+        }
+        else{
+            Toast.makeText(MainActivity.this,"Los campos estan nulos",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void registrar(View view) {
+        String email = correo.getText().toString();
+        String password = contra.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Respuesta", "createUserWithEmail:success");
+                            Toast.makeText(MainActivity.this, "Registrado",
+                                    Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+}
